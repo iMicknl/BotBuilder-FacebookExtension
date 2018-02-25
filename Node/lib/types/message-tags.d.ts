df1ef5768ca67ae0e699a635493bfd251fa661ee
Mick Vleeshouwer <mick@imick.nl>
2018-02-25 18:38:32 +0100

    Update message tags constants

--- Node/lib/types/message-tags.d.ts
+++ Node/lib/types/message-tags.d.ts
@@ -1,7 +1,17 @@
 import { Message } from 'botbuilder';
 export declare const MessageTags: {
+    PAIRING_UPDATE: string;
+    APPLICATION_UPDATE: string;
+    ACCOUNT_UPDATE: string;
+    PAYMENT_UPDATE: string;
+    PERSONAL_FINANCE_UPDATE: string;
     SHIPPING_UPDATE: string;
     RESERVATION_UPDATE: string;
     ISSUE_RESOLUTION: string;
+    APPOINTMENT_UPDATE: string;
+    GAME_EVENT: string;
+    TRANSPORTATION_UPDATE: string;
+    FEATURE_FUNCTIONALITY_UPDATE: string;
+    TICKET_UPDATE: string;
 };
 export declare const AddMessageTag: (message: Message, tag: string) => Message;
--- Node/src/types/message-tags.ts
+++ Node/src/types/message-tags.ts
@@ -6,15 +6,26 @@ import { IMiddlewareMap, Session, Message } from 'botbuilder';
 //=========================================================
 
 export const MessageTags = {
+    PAIRING_UPDATE: 'PAIRING_UPDATE',
+    APPLICATION_UPDATE: 'APPLICATION_UPDATE',
+    ACCOUNT_UPDATE: 'ACCOUNT_UPDATE',
+    PAYMENT_UPDATE: 'PAYMENT_UPDATE',
+    PERSONAL_FINANCE_UPDATE: 'PERSONAL_FINANCE_UPDATE',
     SHIPPING_UPDATE: 'SHIPPING_UPDATE',
     RESERVATION_UPDATE: 'RESERVATION_UPDATE',
-    ISSUE_RESOLUTION: 'ISSUE_RESOLUTION'
+    ISSUE_RESOLUTION: 'ISSUE_RESOLUTION',
+    APPOINTMENT_UPDATE: 'APPOINTMENT_UPDATE',
+    GAME_EVENT: 'GAME_EVENT',
+    TRANSPORTATION_UPDATE: 'TRANSPORTATION_UPDATE',
+    FEATURE_FUNCTIONALITY_UPDATE: 'FEATURE_FUNCTIONALITY_UPDATE',
+    TICKET_UPDATE: 'TICKET_UPDATE'
 };
 
 export const AddMessageTag = (message: Message, tag: string) => {
 
     message.sourceEvent({
         facebook: {
+            messaging_type: 'MESSAGE_TAG',
             tag
         }
     });
