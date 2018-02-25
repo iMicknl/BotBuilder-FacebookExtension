76a7aab1b8c523faf63ea749b425b09a0f09298a
Mick Vleeshouwer <mick@imick.nl>
2018-02-25 18:22:57 +0100

    Export interfaces also

--- Node/src/botbuilder-facebook.ts
+++ Node/src/botbuilder-facebook.ts
@@ -1,6 +1,6 @@
-export { RetrieveUserProfile } from './middleware/user-profile'
-export { CallbackRecognizer } from './recognizers/callback-recognizer';
-export { AddMessageTag, MessageTags } from './types/message-tags';
+export * from './middleware/user-profile'
+export * from './recognizers/callback-recognizer';
+export * from './types/message-tags';
 
 // Deprecated since 1.2.0
 export { CallbackRecognizer as ReferralRecognizer } from './recognizers/callback-recognizer';
